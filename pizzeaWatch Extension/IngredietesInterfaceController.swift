c97aae32ecb3a68360995fda1af21af81f1ce39d
fernando.ransanz <uxama50@hotmail.com>
2015-11-23 09:47:33 +0100

    desarrollo - pantalla de ingredientes sin funcionalidad

---
+++ pizzeaWatch Extension/IngredietesInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  IngredietesInterfaceController.swift
+//  pizzea
+//
+//  Created by subdireccion.archivos.spain@gmail.com on 23/11/15.
+//  Copyright © 2015 Fernando Ransanz. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class IngredietesInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
